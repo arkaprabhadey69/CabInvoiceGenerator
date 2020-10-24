3afa4cc17ad0c8a6498213999d180d03c09eeb08
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-24 12:15:34 +0530

    [Arka]Added:Test methods to return minimum fare and correct fare both

--- src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
+++ src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
@@ -15,4 +15,12 @@ public class InvoiceGeneratorTest {
         double fare=invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(43,fare,0);
     }
+    @Test
+    public void givenDistanceAndTimeReturnCorrectMinimumFare() {
+        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+        double distance=0.1;
+        double time=3;
+        double fare=invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0);
+    }
 }
