fcc19a15aacd3be9b58c79cd665ca33e00f6d54a
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-24 12:50:01 +0530

    [Arka]Added:Enhanced invoice to give totalfare,count of rides and avg price

--- src/main/java/com/bl/invoice/InvoiceGenerator.java
+++ src/main/java/com/bl/invoice/InvoiceGenerator.java
@@ -6,6 +6,7 @@ public class InvoiceGenerator {
     public static int costPerKm = 10;
     public static int costPerMin = 1;
     public static int minimumFare = 5;
+    static int numOfRides=0;
 
     public static void welcome() {
         System.out.println("Welcome to CabInvoiceGenerator");
@@ -22,9 +23,17 @@ public class InvoiceGenerator {
         int fare=0;
         for(Rides ride:rides){
             fare+=this.calculateFare(ride.distance,ride.time);
-
         }
         return fare;
 
+    }
+    public EnhancedInvoice calculateEnhancedInvoice(List<Rides> rides){
+        int fare=0;
+        for(Rides ride:rides){
+            numOfRides++;
+            fare+=this.calculateFare(ride.distance,ride.time);
+        }
+        EnhancedInvoice enhancedInvoice= new EnhancedInvoice(fare,numOfRides);
+        return enhancedInvoice;
     }
 }
