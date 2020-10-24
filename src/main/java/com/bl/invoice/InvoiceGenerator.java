cb0b6ed52f12eb340bcfe76b56980345848bc82b
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-24 12:14:18 +0530

    [Arka]Added:Method to calculate single fare

--- src/main/java/com/bl/invoice/InvoiceGenerator.java
+++ src/main/java/com/bl/invoice/InvoiceGenerator.java
@@ -1,7 +1,19 @@
 package com.bl.invoice;
 
 public class InvoiceGenerator {
-   public static void welcome(){
-       System.out.println("Welcome to CabInvoiceGenerator");
-   }
+    public static int costPerKm = 10;
+    public static int costPerMin = 1;
+    public static int minimumFare = 5;
+
+    public static void welcome() {
+        System.out.println("Welcome to CabInvoiceGenerator");
+    }
+
+    public double calculateFare(double distance, double time) {
+        double fare = (distance * costPerKm + time * costPerMin);
+        if (fare < minimumFare) {
+            return minimumFare;
+        }
+        return fare;
+    }
 }
