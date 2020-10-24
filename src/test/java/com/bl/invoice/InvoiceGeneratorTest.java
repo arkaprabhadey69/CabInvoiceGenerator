6e556b47660cad7a33ca9f4b3dac9c42e2ef7481
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-24 12:50:52 +0530

    [Arka]Added:Test case to check whether correct fare, avg and number of rides returmned

--- src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
+++ src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
@@ -38,4 +38,19 @@ public class InvoiceGeneratorTest {
         System.out.println(fare);
         Assert.assertEquals(157,fare,0);
     }
+    @Test
+    public void givenMultipleRidesShouldReturnCorrectAvgFare() {
+        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+        List<Rides> rides= new ArrayList<>();
+        Rides rides1= new Rides(10,4);
+        Rides rides2= new Rides(5,3);
+        rides.add(rides1);
+        rides.add(rides2);
+        EnhancedInvoice enhancedInvoice=invoiceGenerator.calculateEnhancedInvoice(rides);
+        System.out.println(enhancedInvoice);
+        EnhancedInvoice enhancedInvoice1= new EnhancedInvoice(157,2);
+        Assert.assertEquals(enhancedInvoice1.noOfRides,enhancedInvoice.noOfRides);
+        Assert.assertEquals(enhancedInvoice1.avg,enhancedInvoice.avg,0);
+        Assert.assertEquals(enhancedInvoice1.totalFare,enhancedInvoice.totalFare,0);
+    }
 }
