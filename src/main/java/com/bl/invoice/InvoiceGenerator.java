ae59c696010bf9e435bfba3c55accd5950297457
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-24 14:25:08 +0530

    [Arka]Added:Enhanced invoice to give totalfare,count of rides and avg price of premium or normal

--- src/main/java/com/bl/invoice/InvoiceGenerator.java
+++ src/main/java/com/bl/invoice/InvoiceGenerator.java
@@ -6,33 +6,35 @@ public class InvoiceGenerator {
     public static int costPerKm = 10;
     public static int costPerMin = 1;
     public static int minimumFare = 5;
-    static int numOfRides=0;
-    static int noOfRides;
 
     public static void welcome() {
         System.out.println("Welcome to CabInvoiceGenerator");
     }
 
-    public double calculateFare(double distance, double time) {
-        double fare = (distance * costPerKm + time * costPerMin);
-        if (fare < minimumFare) {
-            return minimumFare;
+    public double calculateFare(double distance, double time, String type)
+    {
+        RideType rideType = new RideType(type);
+        double totalFare = distance * rideType.costPerKilometer + time * rideType.costPerTime;
+        if (totalFare < rideType.minimumFare)
+        {
+            return rideType.minimumFare;
         }
-        return fare;
+        return totalFare;
     }
     public double calculateInvoice(List<Rides> rides){
         int fare=0;
         for(Rides ride:rides){
-            fare+=this.calculateFare(ride.distance,ride.time);
+            fare+=this.calculateFare(ride.distance,ride.time,"normal");
         }
         return fare;
 
     }
     public EnhancedInvoice calculateEnhancedInvoice(List<Rides> rides){
         int fare=0;
+        int numOfRides=0;
         for(Rides ride:rides){
             numOfRides++;
-            fare+=this.calculateFare(ride.distance,ride.time);
+            fare+=this.calculateFare(ride.distance,ride.time,"normal");
         }
         EnhancedInvoice enhancedInvoice= new EnhancedInvoice(fare,numOfRides);
         return enhancedInvoice;
@@ -43,13 +45,36 @@ public class InvoiceGenerator {
             if(userID==null)
                 throw new NullPointerException("Null not allowed");
             if(UserAccount.userRides.containsKey(userID)){
-                double totalfare=0;
+                double totalFare=0;
+                int numRides=0;
+                for(Rides ride:UserAccount.userRides.get(userID)){
+                    numRides++;
+                    totalFare+=this.calculateFare(ride.distance,ride.time,"normal");
+                }
+                EnhancedInvoice invoiceSummary = new EnhancedInvoice(totalFare,numRides);
+                return  invoiceSummary;
+
+            }
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+    public EnhancedInvoice getInvoiceSummaryPremium(String userID){
+        try{
+            if(userID==null)
+                throw new NullPointerException("Null not allowed");
+            if(UserAccount.userRides.containsKey(userID)){
+                double totalFare=0;
+                int noOfRides=0;
                 int count=0;
                 for(Rides ride:UserAccount.userRides.get(userID)){
                     noOfRides++;
-                    totalfare+=this.calculateFare(ride.distance,ride.time);
+                    totalFare+=this.calculateFare(ride.distance,ride.time,ride.rideType);
                 }
-                EnhancedInvoice invoiceSummary = new EnhancedInvoice(totalfare,noOfRides);
+                EnhancedInvoice invoiceSummary = new EnhancedInvoice(totalFare,noOfRides);
                 return  invoiceSummary;
 
             }
