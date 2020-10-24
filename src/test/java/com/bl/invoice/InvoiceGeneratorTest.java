6efa1fe4ab611fb3f2e4f9068030672fcfc0c911
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-24 12:32:26 +0530

    [Arka]Added:Test methods to return correct fare for multiple rides

--- src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
+++ src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
@@ -3,6 +3,9 @@ package com.bl.invoice;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class InvoiceGeneratorTest {
@@ -22,5 +25,17 @@ public class InvoiceGeneratorTest {
         double time=3;
         double fare=invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(5,fare,0);
+    }
+    @Test
+    public void givenMultipleRidesShouldReturnCorrectFare() {
+        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
+        List<Rides> rides= new ArrayList<>();
+        Rides rides1= new Rides(10,4);
+        Rides rides2= new Rides(5,3);
+        rides.add(rides1);
+        rides.add(rides2);
+        double fare=invoiceGenerator.calculateInvoice(rides);
+        System.out.println(fare);
+        Assert.assertEquals(157,fare,0);
     }
 }
