f439c564dc5dee993856e2e0ab7b5ef8ec9d6cb9
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-24 13:31:27 +0530

    [Arka]Added:Method to get invoice by userID

--- src/main/java/com/bl/invoice/InvoiceGenerator.java
+++ src/main/java/com/bl/invoice/InvoiceGenerator.java
@@ -7,6 +7,7 @@ public class InvoiceGenerator {
     public static int costPerMin = 1;
     public static int minimumFare = 5;
     static int numOfRides=0;
+    static int noOfRides;
 
     public static void welcome() {
         System.out.println("Welcome to CabInvoiceGenerator");
@@ -35,5 +36,28 @@ public class InvoiceGenerator {
         }
         EnhancedInvoice enhancedInvoice= new EnhancedInvoice(fare,numOfRides);
         return enhancedInvoice;
+    }
+
+    public EnhancedInvoice getInvoiceSummary(String userID){
+        try{
+            if(userID==null)
+                throw new NullPointerException("Null not allowed");
+            if(UserAccount.userRides.containsKey(userID)){
+                double totalfare=0;
+                int count=0;
+                for(Rides ride:UserAccount.userRides.get(userID)){
+                    noOfRides++;
+                    totalfare+=this.calculateFare(ride.distance,ride.time);
+                }
+                EnhancedInvoice invoiceSummary = new EnhancedInvoice(totalfare,noOfRides);
+                return  invoiceSummary;
+
+            }
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+        }
+        return null;
+
     }
 }
