ca68eb01bc165110eba84d3c8356350d123da452
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-26 10:03:14 +0530

    [Arka]Refactored:Defined constructor to initialise hashmap, works as ride repository

--- src/main/java/com/bl/invoice/UserAccount.java
+++ src/main/java/com/bl/invoice/UserAccount.java
@@ -3,11 +3,16 @@ package com.bl.invoice;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+//Acts as Repository to add rides to a particular user-ID
 
 public class UserAccount {
-    public static Map<String, List<Rides>> userRides=new HashMap<>();
+    public static Map<String, List<Rides>> userRides;
 
-    public static void addRide(String userID, List<Rides> rides) {
+    public UserAccount() {
+        userRides = new HashMap<>();
+    }
+//Method to Add rides
+    public void addRide(String userID, List<Rides> rides) {
         try {
             if (userID == null || rides == null) {
                 throw new NullPointerException("Null value not allowed");
