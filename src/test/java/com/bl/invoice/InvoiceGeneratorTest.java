0a09ee70abf37a9e99f28b89a7dc8e7caca21aeb
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-26 09:03:37 +0530

    [Arka]Refactored:Added Before methods

--- src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
+++ src/test/java/com/bl/invoice/InvoiceGeneratorTest.java
@@ -1,6 +1,7 @@
 package com.bl.invoice;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -9,78 +10,68 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class InvoiceGeneratorTest {
+    InvoiceGenerator invoiceGenerator = null;
+
+    @Before
+    public void setup() {
+        invoiceGenerator = new InvoiceGenerator();
+    }
 
     @Test
     public void givenDistanceAndTimeReturnCorrectFare() {
-        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-        double distance=4;
-        double time=3;
-        double fare=invoiceGenerator.calculateFare(distance,time,"normal");
-        Assert.assertEquals(43,fare,0);
+        double distance = 4;
+        double time = 3;
+        double fare = invoiceGenerator.calculateFare(distance, time, "normal");
+        Assert.assertEquals(43, fare, 0);
     }
+
     @Test
     public void givenDistanceAndTimeReturnCorrectMinimumFare() {
-        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-        double distance=0.1;
-        double time=3;
-        double fare=invoiceGenerator.calculateFare(distance,time,"normal");
-        Assert.assertEquals(5,fare,0);
+        double distance = 0.1;
+        double time = 3;
+        double fare = invoiceGenerator.calculateFare(distance, time, "normal");
+        Assert.assertEquals(5, fare, 0);
     }
+
     @Test
     public void givenMultipleRidesShouldReturnCorrectFare() {
-        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-        List<Rides> rides= new ArrayList<>();
-        Rides rides1= new Rides(10,4);
-        Rides rides2= new Rides(5,3);
+        List<Rides> rides = new ArrayList<>();
+        Rides rides1 = new Rides(10, 4);
+        Rides rides2 = new Rides(5, 3);
         rides.add(rides1);
         rides.add(rides2);
-        double fare=invoiceGenerator.calculateInvoice(rides);
+        double fare = invoiceGenerator.calculateInvoice(rides);
         System.out.println(fare);
-        Assert.assertEquals(157,fare,0);
+        Assert.assertEquals(157, fare, 0);
     }
+
     @Test
     public void givenMultipleRidesShouldReturnCorrectAvgFare() {
-        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-        List<Rides> rides= new ArrayList<>();
-        Rides rides1= new Rides(10,4);
-        Rides rides2= new Rides(5,3);
+        List<Rides> rides = new ArrayList<>();
+        Rides rides1 = new Rides(10, 4);
+        Rides rides2 = new Rides(5, 3);
         rides.add(rides1);
         rides.add(rides2);
-        EnhancedInvoice enhancedInvoice=invoiceGenerator.calculateEnhancedInvoice(rides);
-        EnhancedInvoice enhancedInvoice1= new EnhancedInvoice(157,2);
-        Assert.assertEquals(enhancedInvoice1.noOfRides,enhancedInvoice.noOfRides);
-        Assert.assertEquals(enhancedInvoice1.avg,enhancedInvoice.avg,0);
-        Assert.assertEquals(enhancedInvoice1.totalFare,enhancedInvoice.totalFare,0);
+        EnhancedInvoice enhancedInvoice = invoiceGenerator.calculateEnhancedInvoice(rides);
+        EnhancedInvoice enhancedInvoice1 = new EnhancedInvoice(157, 2);
+        Assert.assertEquals(enhancedInvoice1.noOfRides, enhancedInvoice.noOfRides);
+        Assert.assertEquals(enhancedInvoice1.avg, enhancedInvoice.avg, 0);
+        Assert.assertEquals(enhancedInvoice1.totalFare, enhancedInvoice.totalFare, 0);
     }
-    @Test
-    public void givenMultipleRidesShouldReturnCorrectFareUsingUserID() {
-        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-        List<Rides> rides= new ArrayList<>();
-        Rides rides1= new Rides(10,4,"normal");
-        Rides rides2= new Rides(5,3,"normal");
-        rides.add(rides1);
-        rides.add(rides2);
-        UserAccount.addRide("Shyam",rides);
-        EnhancedInvoice enhancedInvoice=invoiceGenerator.getInvoiceSummary("Shyam");
-        EnhancedInvoice enhancedInvoice1= new EnhancedInvoice(177,2);
-        System.out.println(enhancedInvoice);
-       // Assert.assertEquals(enhancedInvoice1.noOfRides,enhancedInvoice.noOfRides);
-        //Assert.assertEquals(enhancedInvoice1.avg,enhancedInvoice.avg,0);
-        //Assert.assertEquals(enhancedInvoice1.totalFare,enhancedInvoice.totalFare,0);
-    }
+
     @Test
     public void givenMultipleRidesShouldReturnCorrectInvoicePremium() {
-        InvoiceGenerator invoiceGenerator= new InvoiceGenerator();
-        List<Rides> rides= new ArrayList<>();
-        Rides rides1= new Rides(10,4,"premium");
-        Rides rides2= new Rides(5,3,"normal");
+        List<Rides> rides = new ArrayList<>();
+        Rides rides1 = new Rides(10, 4, "premium");
+        Rides rides2 = new Rides(5, 3, "normal");
         rides.add(rides1);
         rides.add(rides2);
-        UserAccount.addRide("Shyam",rides);
-        EnhancedInvoice enhancedInvoice=invoiceGenerator.getInvoiceSummaryPremium("Shyam");
-        EnhancedInvoice enhancedInvoice1= new EnhancedInvoice(211,2);
-        Assert.assertEquals(enhancedInvoice1.noOfRides,enhancedInvoice.noOfRides);
-        Assert.assertEquals(enhancedInvoice1.avg,enhancedInvoice.avg,0);
-        Assert.assertEquals(enhancedInvoice1.totalFare,enhancedInvoice.totalFare,0);
+        UserAccount.addRide("Shyam", rides);
+        UserAccount.addRide("Ram", rides);
+        EnhancedInvoice enhancedInvoice = invoiceGenerator.getInvoiceSummaryPremium("Ram");
+        EnhancedInvoice enhancedInvoice1 = new EnhancedInvoice(211, 2);
+        Assert.assertEquals(enhancedInvoice1.noOfRides, enhancedInvoice.noOfRides);
+        Assert.assertEquals(enhancedInvoice1.avg, enhancedInvoice.avg, 0);
+        Assert.assertEquals(enhancedInvoice1.totalFare, enhancedInvoice.totalFare, 0);
     }
 }
